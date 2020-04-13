c940c940c8d153c8669f9952f2ea1f3f65ceb36e
guitarmike1966 <guitarmike1966@gmail.com>
2020-04-13 16:39:50 -0500

    finish changes to calculation process

--- MandelbrotOSX3/ViewController.swift
+++ MandelbrotOSX3/ViewController.swift
@@ -8,13 +8,13 @@
 
 import Cocoa
 
-let WIDTH = 1024
-let HEIGHT = 768
+let WIDTH = 800
+let HEIGHT = 600
 
-let startWidth = 1.0 - -2.0
-let startHeight = 1.25 - -1.25
+// let startWidth = 1.0 - -2.0
+// let startHeight = 1.25 - -1.25
 
-let rate = 0.012
+let Rate = 0.0012
 
 let startXA = -2.0
 let startXB = 1.0
@@ -51,54 +51,48 @@ class ViewController: NSViewController {
 
     @IBAction func StartButtonClick(_ sender: Any) {
 
-        for x in 0..<6 {
+        for x in 6..<12 {
 
-//            self.MandelbrotImageView.createImage()
+            (xxa,xxb) = calc(a: startXA, b: startXB, iter: x, rate: Rate, portionA: 0.05, portionB: 0.95)
+            (xya,xyb) = calc(a: startYA, b: startYB, iter: x, rate: Rate, portionA: 0.5, portionB: 0.5)
 
-//            let downloadURL = FileManager.default.urls(for: .downloadsDirectory, in: .userDomainMask).first!
+            self.MandelbrotImageView.xa = xxa
+            self.MandelbrotImageView.xb = xxb
+            self.MandelbrotImageView.ya = xya
+            self.MandelbrotImageView.yb = xyb
+
+            self.MandelbrotImageView.createImage()
+
+            let downloadURL = FileManager.default.urls(for: .downloadsDirectory, in: .userDomainMask).first!
+
             self.imageCounter += 1
-//            let fileName = String(format: "new-mandelbrot-%04d.png", self.imageCounter)
-//            let destinationURL = downloadURL.appendingPathComponent(fileName)
-//            let saveResult = self.MandelbrotImageView.image!.pngWrite(to: destinationURL)
-
-            print("\nImage: \(imageCounter-1)")
-            let oldValues = String(format: "xa: %1.17f  xb: %1.17f  ya: %1.17f  yb: %1.17f",
-                                   MandelbrotImageView.xa,MandelbrotImageView.xb,MandelbrotImageView.ya,MandelbrotImageView.yb)
-            print("  Old: \(oldValues)")
-
-            // new calculation
-            let newValues = String(format: "xa: %1.17f  xb: %1.17f  ya: %1.17f  yb: %1.17f",
-                                   self.xxa,self.xxb,self.xya,self.xyb)
-            print("  New: \(newValues)")
-
-
-            let xWidth = self.MandelbrotImageView.xb - self.MandelbrotImageView.xa
-            let xHeight = self.MandelbrotImageView.yb - self.MandelbrotImageView.ya
-
-            let xShrink = xWidth * 0.00012
-            let yShrink = xHeight * 0.00012
-
-            self.MandelbrotImageView.xa += (xShrink * 0.5)
-            self.MandelbrotImageView.xb -= (xShrink * 9.5)
-            self.MandelbrotImageView.ya += (yShrink * 5)
-            self.MandelbrotImageView.yb -= (yShrink * 5)
-
-            // new calculation
-            let xaMove = startWidth*pow((1 - rate), Double(imageCounter))
-            
-            let xbMove = startHeight*pow((1 + rate), Double(imageCounter))
-
-            // calculate here the new xa,xb and ya,yb
-            //
-            let tempValue = String(format: "%1.17f                                %1.17f",xaMove,xbMove)
-            print("     : \(tempValue)")
+            let fileName = String(format: "new-mandelbrot-%04d.png", self.imageCounter)
+            let destinationURL = downloadURL.appendingPathComponent(fileName)
+            let saveResult = self.MandelbrotImageView.image!.pngWrite(to: destinationURL)
 
             DispatchQueue.main.async {
-                self.ImageCountLabel.stringValue = String(format: "%04d", self.imageCounter)
+                self.ImageCountLabel.stringValue = String(format: "%05d", self.imageCounter)
             }
 
         }
     }
+    
+    
+    private func calc(a: Double, b: Double, iter: Int, rate: Double, portionA: Double, portionB: Double) -> (Double,Double) {
+        var aReturn: Double
+        var bReturn: Double
+
+        let Width = b - a
+        let newWidth = Width * pow((1 - (rate)),Double(iter))
+
+        let aChange = (Width - newWidth) * portionA
+        let bChange = (Width - newWidth) * portionB
+        aReturn = a + aChange
+        bReturn = b - bChange
+
+        return (aReturn,bReturn)
+    }
+
     
 }
 
